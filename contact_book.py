810c12cdd5e71b3fe2b89d10629836dd381dc362
Daniel <danirsocial1183@gmail.com>
2024-01-12 20:21:18 -0300

    fixing integer validation errors

--- contact_book.py
+++ contact_book.py
@@ -2,13 +2,31 @@ contacts = []
 
 def add_contact():
     while True:
-        print('Enter "exit" to stop.')
+        print('\nEnter "exit" to stop.')
         name = str(input('Name: ')).strip().capitalize()
         if name.lower() == 'exit':
             break
-        number = int(input('Tel: '))
 
-        novo_contato = {'name': name, 'number': number}
+        while True:
+            try:
+                number = int(input('Number: '))
+                break
+            except ValueError:
+                print('Please enter a valid number.')
+
+        print('Enter "no" to exclude the addition of an email.')
+        while True:
+            email = input('E-mail: ').strip().lower()
+            if email == '':
+                break
+            elif email == 'no':
+                break
+            if '@' not in email or '.' not in email:
+                print('Please enter a valid email or type "no" to exclude.')
+            else:
+                break
+
+        novo_contato = {'name': name, 'number': number, 'email': email}
         contacts.append(novo_contato)
 
 
@@ -16,9 +34,12 @@ while True:
     print('CONTACT BOOK 📒')
     print('-' * 30)
     for contact in contacts:
-        print(f"Name: {contact['name']} \nPhone number: {contact['number']}\n")
+        print(f"Name: {contact['name']} \nPhone number: {contact['number']} \nE-mail address: {contact['email']}\n")
     print('Enter 1: + new contact')
-    menu = int(input('Option: '))
-    if menu == 1:
-        add_contact()
-    print('-' * 30)
+    try:
+        menu = int(input('Option: '))
+        if menu == 1:
+            add_contact()
+        print('-' * 30)
+    except ValueError:
+        print('Please enter a valid option.')
