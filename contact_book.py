a1c481d950c93b2de27b369b484884efbb2d79bc
Daniel <danirsocial1183@gmail.com>
2024-01-12 19:33:12 -0300

    first commit

---
+++ contact_book.py
@@ -0,0 +1,23 @@
+contacts = []
+
+def add_contact():
+    while True:
+        name = str(input('Name: ')).strip().capitalize()
+        if name.lower() == 'exit':
+            break
+        number = int(input('Tel: '))
+
+        novo_contato = {'name': name, 'number': number}
+        contacts.append(novo_contato)
+
+
+while True:
+    print('CONTACT BOOK 📒')
+    print('-' * 30)
+    for contact in contacts:
+        print(f"Name: {contact['name']} \nPhone number: {contact['number']}\n")
+    print('Enter 1: + new contact')
+    menu = int(input('Option: '))
+    if menu == 1:
+        add_contact()
+    print('-' * 30)
